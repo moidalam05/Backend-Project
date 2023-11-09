1497fb48a950c07be1d257915dcd684bc339d2e5
moid786 <moid.30400@davietjal.or>
2023-11-09 16:12:50 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+// import app from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server Started on port: ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed!! ",error);
+    })
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requstHandler) => {(req, res, next) => {
+    Promise.resolve(requstHandler(req, res, next)).catch((err) => next(err))};
+};
+
+export { asyncHandler };
+
+    
+
+
+
+
+
+
+
+
+
+    
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message:err.message
+//         })
+//     }
+// };
