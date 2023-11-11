4a77861f3a85d2c7800c4f06df63584f58f24f4b
moid786 <moid.30400@davietjal.or>
2023-11-11 23:55:18 +0530

    file upload setup with multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary} from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        //upload the file on cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        })
+        // file has been uploaded successfully
+        console.log("File is uploded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the uplodede opretaion failed
+
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+// cloudinary.v2.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
