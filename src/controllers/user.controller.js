779bddc6e4229c5b1caeff1af85ed97fc6786f9f
moid786 <moid.30400@davietjal.or>
2023-12-20 16:29:47 +0530

    Update refresh token logic.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -180,7 +180,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request");
   }
 
